d06fe23582d6ef215219a59d03a8e514ac6a25f8
Matheus Oliveira <matheus7op@gmail.com>
2023-02-12 13:28:53 -0300

    ref: fix identation on index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 
 const clearFile = (filePath) => {
     if(fs.existsSync(filePath)) {
-    fs.unlink(filePath, (err) => {
-        if(err) console.error(err);
-    });
-}
+        fs.unlink(filePath, (err) => {
+            if(err) console.error(err);
+        });
+    }
 }
 
 const getInvestmentData = (investment) => {
@@ -29,7 +29,8 @@ const getInvestmentData = (investment) => {
             break;
         default:
             throw new Error(
-                'Behavior for investment type ' + investment.securityType + ' not implemented in this crawler.');
+                'Behavior for investment type ' + investment.securityType + 
+                ' not implemented in this crawler.');
     }
 
     data.delta = ((data.currentValue / data.investedCapital) - 1) * 100.0;
